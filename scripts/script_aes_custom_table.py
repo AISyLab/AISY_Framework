8dc3a7f6d283ce59ab53627ddba74525196ed79c
Guilherme Perin <5265259-ches_submission_dl_mia@users.noreply.gitlab.com>
2021-03-08 14:20:20 +0100

    custom_table script fix

--- scripts/script_aes_custom_table.py
+++ scripts/script_aes_custom_table.py
@@ -3,20 +3,17 @@ from aisy.sca_deep_learning_aes import AisyAes
 from custom.custom_tables.tables import *
 
 aisy = AisyAes()
-aisy.set_datasets_root_folder("D:/traces/")
 aisy.set_dataset("ascad-variable.h5")
 aisy.set_database_name("database_ascad.sqlite")
-
-db_inserts = aisy.get_db_inserts()
-
 aisy.set_aes_leakage_model(leakage_model="HW", byte=2)
 aisy.set_number_of_profiling_traces(10000)
 aisy.set_number_of_attack_traces(1000)
 aisy.set_batch_size(400)
-aisy.set_epochs(50)
+aisy.set_epochs(20)
 aisy.set_neural_network(mlp)
 
 aisy.run()
 
+db_inserts = aisy.get_db_inserts()
 new_insert = CustomTable(value1=10, value2=20, value3=30)
 db_inserts.custom_insert(new_insert)
