e4190dfd34916bdb84df4059ae6918b91eced771
Guilherme Perin <5265259-ches_submission_dl_mia@users.noreply.gitlab.com>
2021-03-23 20:50:13 +0100

    small fix on aes128 leakage model

--- scripts/script_aes_cpa.py
+++ scripts/script_aes_cpa.py
@@ -8,8 +8,9 @@ aisy.set_database_root_folder(databases_root_folder)
 aisy.set_datasets_root_folder(datasets_root_folder)
 aisy.set_database_name("database_ascad.sqlite")
 aisy.set_dataset(datasets_dict["hw_aes.h5"])
+target_byte = 0
 aisy.set_aes_leakage_model(leakage_model="HW",
-                           byte=0,
+                           byte=target_byte,
                            direction="Encryption",
                            cipher="AES128",
                            round=10,
@@ -20,12 +21,13 @@ correlation, key_rank_evolution_cpa = aisy.run(key_rank_report_interval=key_rank
 correlation = correlation.T
 
 known_key = datasets_dict["hw_aes.h5"]["key"]
+known_key_int = [int(x) for x in bytearray.fromhex(known_key)]
 
 plt.subplot(1, 2, 1)
 for kg in range(256):
-    plt.plot(correlation[kg], color='grey')
-known_key_int = [int(x) for x in bytearray.fromhex(known_key)]
-plt.plot(correlation[known_key_int[0]], 'r')
+    if kg != known_key_int[target_byte]:
+        plt.plot(correlation[kg], color='grey')
+plt.plot(correlation[known_key_int[target_byte]], 'r')
 
 plt.subplot(1, 2, 2)
 plt.plot(key_rank_evolution_cpa, color='green')
